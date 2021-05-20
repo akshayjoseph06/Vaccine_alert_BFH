8477136d4cec526fabeb941cd21b56aad1009616
Akshay Joseph <47769124+akshayjoseph666@users.noreply.github.com>
2021-05-20 17:25:51 +0530

    basic function

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-import requests
-url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date=31-03-2021'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
-result = requests.get(url, headers=headers)
-print(result.json())
+import os
+os.system('cmd /k "ffmpeg -i test.mp3 -filter_complex "[0:a]showwaves=s=1280x720:mode=cline,format=yuv420p[v]" -map "[v]" -map 0:a -c:v libx264 -c:a copy test.mp4"') # 128x96
