90c87ae4c2c22f2ead6162595b9ed6de418f7ba8
Akshay Joseph <47769124+akshayjoseph666@users.noreply.github.com>
2021-05-20 17:27:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 import os
-os.system('cmd /k "ffmpeg -i test.mp3 -filter_complex "[0:a]showwaves=s=1280x720:mode=cline,format=yuv420p[v]" -map "[v]" -map 0:a -c:v libx264 -c:a copy test.mp4"') # 128x96
+os.system('cmd /k "ffmpeg -i test.mp3 -filter_complex "[0:a]showwaves=s=128x96:mode=cline,format=yuv420p[v]" -map "[v]" -map 0:a -c:v libx264 -c:a copy test.mp4"') # 128x96
+
+#1280x720
